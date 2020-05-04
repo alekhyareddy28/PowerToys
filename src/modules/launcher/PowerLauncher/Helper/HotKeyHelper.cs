573f674c886bce4ed5d5fcc021a6b1f7bb7f7464
Alekhya Reddy <reddykalekhya@gmail.com>
2020-05-04 08:14:34 -0700

    Created empty helper class

---
+++ src/modules/launcher/PowerLauncher/Helper/HotKeyHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerLauncher.Helper
+{
+    public class HotKeyHelper : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
